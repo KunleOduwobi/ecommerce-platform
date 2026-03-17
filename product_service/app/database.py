466f424c77837eef3e9dfbbd061941c3c5059a48
KunleOduwobi <kunleodu@gmail.com>
2026-03-17 13:07:58 +0100

    Dockerize the API + PostgreSQL

--- product_service/app/database.py
+++ product_service/app/database.py
@@ -1,9 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/ecommerce"
 # DATABASE_URL = "postgresql://localhost/ecommerce"
-DATABASE_URL = "postgresql://ecommerce_user:mypassword123@localhost:5432/ecommerce"
+# DATABASE_URL = "postgresql://ecommerce_user:mypassword123@localhost:5432/ecommerce"
+DATABASE_URL = os.getenv("DATABASE_URL",
+                         "postgresql://ecommerce_user:mypassword123@localhost:5432/ecommerce")
 
 engine = create_engine(DATABASE_URL)
 
